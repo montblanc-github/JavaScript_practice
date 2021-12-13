33d61f068afccbefcd777ac87252db99f1977067
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-13 18:48:51 +0900

    [Add] スコープ学習

--- JavaScript変数宣言/practice-variable/sample.js
+++ JavaScript変数宣言/practice-variable/sample.js
@@ -33,3 +33,34 @@
 // console.log(nickname);
 // nickname = "jiro";
 // console.log(nickname);
+
+// // スコープ学習
+// varのスコープ
+// var str = "webcamp"; //グローバルスコープ
+
+// function foo() {
+//   console.log(str);
+//   var y = "hello"; //関数スコープ
+// }
+
+// foo();
+// console.log(y);
+
+// letのスコープ(ブロック内がスコープ、constも同じブロックスコープ)
+// function foo() {
+//   let x = "webcamp";
+//   {
+//     let y = "hello webcamp";
+//   }
+//   console.log(x);
+//   console.log(y);
+// }
+
+// foo();
+
+// 別パターン
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// console.log(i);
