46155175bdc4599e1dacaa67b37142ab8f041ef4
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-13 17:24:17 +0900

    [Add] 繰り返し処理

--- JavaScript2章/app.js
+++ JavaScript2章/app.js
@@ -35,13 +35,27 @@
 // alert(str1 + str2);
 
 // 条件分岐を用いて、りんごとみかんの値段を比較
-let orange = 100;
-let apple = 200;
+// let orange = 100;
+// let apple = 200;
 
-if (orange < apple) {
-  alert("みかんの値段がりんごより安い");
-} else if (orange == apple) {
-  alert("みかんとりんごが同じ値段");
-} else {
-  alert("みかんの値段がりんごより高い");
+// if (orange < apple) {
+// alert("みかんの値段がりんごより安い");
+// } else if (orange == apple) {
+// alert("みかんとりんごが同じ値段");
+// } else {
+// alert("みかんの値段がりんごより高い");
+// }
+
+// 繰り返し処理
+let max = 100;
+let num = 1;
+let count = 0;
+
+while (num < max) {
+  num = num * 2;
+  count = count + 1;
 }
+
+alert(
+  "2を掛け続けて" + max + "を超えるのに必要だった回数は" + count + "回です"
+);
