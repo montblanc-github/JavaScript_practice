d6d26e360c0c4b6f48018d641bec437dc9ce80d5
montblanc-github <86041660+montblanc-github@users.noreply.github.com>
2021-12-13 17:10:25 +0900

    [Add] 変数:文字列の結合

--- JavaScript2章/app.js
+++ JavaScript2章/app.js
@@ -1,2 +1,30 @@
+// 文字をアラートウィンドウに表示する
 let hello = "Hello World";
-alert(hello);
+// alert(hello);
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = "JavaScriptを覚えよう";
+
+// 足し算
+// alert(4 + 3);
+
+// 引き算
+// alert(8 - 5);
+
+// 掛け算
+// alert(2 * 6);
+
+// 割り算
+// alert(10 / 2);
+
+// 文字列を結合する
+alert("Hello" + "World");
